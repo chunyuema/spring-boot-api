06138bb7591ba18f513fa0d99d8abcce2a39f7ba
Chunyue Ma <cma01@wesleyan.edu>
2021-07-16 18:37:22 -0400

    add student Model and upgrade API to render json for student

--- src/main/java/com/chunyue/springbootapi/SpringBootApiApplication.java
+++ src/main/java/com/chunyue/springbootapi/SpringBootApiApplication.java
@@ -1,28 +1,36 @@
 package com.chunyue.springbootapi;
 
+import com.chunyue.springbootapi.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class SpringBootApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiApplication.class, args);
+    }
 
 //	@GetMapping
 //	public String hello(){
 //		return "Hello World";
 //	}
 
-	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
-	}
-
+    @GetMapping
+    public List<Student> hello() {
+        return List.of(
+                new Student(1L,
+                        "Chunyue",
+                        "cma01@gmail",
+                        LocalDate.of(1998, Month.APRIL, 4),
+                        22)
+        );
+    }
 }
