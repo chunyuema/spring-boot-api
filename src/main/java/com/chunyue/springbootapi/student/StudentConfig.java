163723d3ffea3b7a5ed93399564018718f2d3dff
Chunyue Ma <cma01@wesleyan.edu>
2021-07-17 15:39:02 -0400

    initialize db and connect to db with repository

---
+++ src/main/java/com/chunyue/springbootapi/student/StudentConfig.java
@@ -0,0 +1,29 @@
+package com.chunyue.springbootapi.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.JANUARY;
+import static java.time.Month.JULY;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student chunyue = new Student(1L, "chunyuema", "cma01@gmail.com",
+                    LocalDate.of(2000, JANUARY, 23),
+                    21);
+
+            Student alex = new Student( "alex", "alogan@gmail.com",
+                    LocalDate.of(2000, JULY, 23),
+                    21);
+            repository.saveAll(List.of(chunyue, alex));
+        };
+    }
+}
